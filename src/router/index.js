d7b9e0e06d05b09ea3f10f44b1d90edcde1c47cc
Nguyễn Thị Quỳnh Hương <iris.nguyen1309.qh@gmail.com>
2020-12-05 13:09:58 +0700

    Update components, router, views

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+
+let baseRoutes = [
+  
+]
+
+const router = new Router({
+  mode: 'history',
+  linkExactActiveClass: 'active',
+  base: process.env.BASE_URL,
+  routes: baseRoutes
+})
+
+export default router
