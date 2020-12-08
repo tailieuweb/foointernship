c8ba0e3b982e7a12f0bf7f7c99e85524b588e1ef
lethanhhung275 <33917744+lethanhhung275@users.noreply.github.com>
2020-12-08 08:21:11 +0700

    Create PopupNopBC.vue

---
+++ src/components/QuanLyBaoCao/PopupNopBC.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-dialog max-width="290px">
+        <v-btn flat slot="activator" class="success">Nộp Báo Cáo</v-btn>
+        <v-card>
+            <v-card-title>
+                <h2>Nộp Báo Cáo</h2>
+            </v-card-title>
+            <v-divider></v-divider>
+        
+            <v-card-text>
+                <v-form class="px-3">
+                   <v-file-input show-size label="Chọn file"></v-file-input>
+                </v-form>
+            </v-card-text>
+            <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Nộp
+          </v-btn>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Hủy
+          </v-btn>
+        </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+<script>
+export default {
+    data: () => ({
+      dialog: false,
+    }),
+}
+</script>
