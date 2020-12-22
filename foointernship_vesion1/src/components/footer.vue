5312e1858861a01c8b83de539f3743ecd0f5581f
minhtuyen25092000 <73813278+minhtuyen25092000@users.noreply.github.com>
2020-12-22 12:21:37 +0700

    SetupCode

---
+++ foointernship_vesion1/src/components/footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+ <!-- footer -->
+    <div class="footer">
+      <div class="text-left1">
+        <h3>Trường Cao Đẳng Công Nghệ Thủ Đức</h3>
+      </div>
+      <div class="text-center2">
+        <div class="khung1">
+          <h3>Khoa công nghệ thông tin</h3>
+        </div>
+      </div>
+      <div class="text-right3">
+        <h3>Copyright (©) Team F,H,J</h3>
+      </div>
+    </div>
+    <!-- footer -->
+  </v-app>
+</template>
+<style>
+.text-left1 .h3 {
+  padding-top: 5000px;
+}
+.footer {
+  margin-top: 50px;
+  background: #dae8fc;
+}
+.text-right3 {
+  padding-left: 1000px;
+  margin-top: -39px;
+}
+.text-center2 {
+  padding-left: 550px;
+  margin-top: -36px;
+}
+.theme--dark.v-btn:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined) {
+  background-color: #564fef !important;
+}
+</style>
