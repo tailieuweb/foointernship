ba65414bd0a1b4b7bf6d42c743642ab59ee19140
LuongPhuc <phucphan219@gmail.com>
2020-12-23 09:55:42 +0700

    phuc

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import ListGV from '../components/DanhSachGiangVien/ListGV'
+import ListKhoa from '../components/khoa/ListKhoa'
 import VueAxios from 'vue-axios';
 import axios from 'axios';
 Vue.use(VueAxios,axios);
@@ -10,8 +10,8 @@ Vue.use(Router)
 const routes = [
   {
     path: '/',
-    name: 'ListGV',
-    component: ListGV
+    name: 'ListKhoa',
+    component: ListKhoa
   },
 ]
 
